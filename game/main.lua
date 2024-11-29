13085d9d7b1fd7bc03caba0a38590af2f3736b17
Kizik <84981131+ikiziki@users.noreply.github.com>
2024-11-28 22:01:47 -0500

    Windows compatibility check

--- game/main.lua
+++ game/main.lua
@@ -1,10 +1,14 @@
+-- launcher
+-- chris geese @ 2024
+
 function love.load()
 end
 
 function love.update(dt)
-	print("Update Time: " .. dt)
 end
 
 function love.draw()
-	love.graphics.print("Hello World!", 400, 300)
+end
+
+function love.mousepressed(x, y, button)
 end
